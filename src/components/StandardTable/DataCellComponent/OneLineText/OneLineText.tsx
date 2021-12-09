bd2c408c6bbd7711e19dcb42d7db6701906a50e4
khangdm <minhkhang231199@gmail.com>
2021-12-09 10:30:09 +0700

    update one line text

--- src/components/StandardTable/DataCellComponent/OneLineText/OneLineText.tsx
+++ src/components/StandardTable/DataCellComponent/OneLineText/OneLineText.tsx
@@ -7,24 +7,50 @@ export interface OneLineTextProps {
   avatar?: string;
   icon?: string;
   name?: string;
+  tableSize?: "large" | "medium" | "small";
 }
 
 function OneLineText(props: OneLineTextProps) {
-  const { className, avatar, name, icon } = props;
+  const { className, avatar, name, icon, tableSize } = props;
   return (
     <>
-      <div className={className}>
-        {avatar && (
-          <img src={avatar} className="avatar-one-line-text" alt="avatar" />
-        )}
-        {icon && <i className={classNames(icon, `icon-one-line-text`)}></i>}
-        {name}
-      </div>
+      {avatar && (
+        <div
+          className={classNames(
+            className,
+            `one-line-image-text-table-size-${tableSize}`
+          )}
+        >
+          {avatar && tableSize !== "small" && (
+            <img
+              src={avatar}
+              className={classNames(
+                `avatar-one-line-text-table-size-${tableSize}`
+              )}
+              alt="avatar"
+            />
+          )}
+          {icon && <i className={classNames(icon, `icon-one-line-text`)}></i>}
+          {name}
+        </div>
+      )}
+      {!avatar && (
+        <div
+          className={classNames(
+            className,
+            `one-line-text-table-size-${tableSize}`
+          )}
+        >
+          {icon && <i className={classNames(icon, `icon-one-line-text`)}></i>}
+          {name}
+        </div>
+      )}
     </>
   );
 }
 OneLineText.defaultProps = {
   avatar: null,
   icon: null,
+  tableSize: "large",
 };
 export default OneLineText;

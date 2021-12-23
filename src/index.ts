5c88765b789815a745d56499d0790a4e9606a81d
DESKTOP-3C5HDO7\LeAnh <darkcometrat1011@gmail.com>
2021-12-23 10:24:37 +0700

    minor fix build

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import FormItem from "components/FormItem/FormItem";
+import FormItem from "./components/FormItem/FormItem";
 
-import InputText from "components/Input/InputText/InputText";
+import InputText from "./components/Input/InputText/InputText";
 import InputNumber from "./components/Input/InputNumber/InputNumber";
 import InputSelect from "./components/Input/InputSelect/InputSelect";
 import TextArea from "./components/TextArea/TextArea";
 
 import Select from "./components/Select/SingleSelect/Select";
-import MultipleSelect from "components/Select/MultipleSelect";
+import MultipleSelect from "./components/Select/MultipleSelect";
 import Tree from "./components/Tree/Tree";
 import TreeSelect from "./components/TreeSelect/TreeSelect";
 
-import AdvanceIdFilterMaster from "components/AdvanceFilterMaster/AdvanceIdFilterMaster";
-import AdvanceMultipleIdFilterMaster from "components/AdvanceFilterMaster/AdvanceMultipleIdFilterMaster";
+import AdvanceIdFilterMaster from "./components/AdvanceFilterMaster/AdvanceIdFilterMaster";
+import AdvanceMultipleIdFilterMaster from "./components/AdvanceFilterMaster/AdvanceMultipleIdFilterMaster";
 
-import StandardTable from "components/StandardTable/StandardTable";
-import ActionBarComponent from "components/StandardTable/ActionBarComponent";
-import LayoutCell from "components/StandardTable/LayoutCell";
-import LayoutHeader from "components/StandardTable/LayoutHeader";
-import Pagination from "components/StandardTable/Pagination";
-import BadgeText from "components/StandardTable/DataCellComponent/BadgeText";
-import OneLineText from "components/StandardTable/DataCellComponent/OneLineText";
-import TwoLineText from "components/StandardTable/DataCellComponent/TwoLineText";
-import StatusLine from "components/StandardTable/DataCellComponent/StatusLine";
+import StandardTable from "./components/StandardTable/StandardTable";
+import ActionBarComponent from "./components/StandardTable/ActionBarComponent";
+import LayoutCell from "./components/StandardTable/LayoutCell";
+import LayoutHeader from "./components/StandardTable/LayoutHeader";
+import Pagination from "./components/StandardTable/Pagination";
+import BadgeText from "./components/StandardTable/DataCellComponent/BadgeText";
+import OneLineText from "./components/StandardTable/DataCellComponent/OneLineText";
+import TwoLineText from "./components/StandardTable/DataCellComponent/TwoLineText";
+import StatusLine from "./components/StandardTable/DataCellComponent/StatusLine";
 
 export {
   FormItem,

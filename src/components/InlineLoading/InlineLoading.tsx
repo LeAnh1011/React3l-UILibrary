ddeaed85954488c4a8e4983dd6be317559fb52c2
khangdm <minhkhang231199@gmail.com>
2021-12-31 15:21:23 +0700

    update inlineLoading

--- src/components/InlineLoading/InlineLoading.tsx
+++ src/components/InlineLoading/InlineLoading.tsx
@@ -3,35 +3,45 @@ import React, { PropsWithChildren } from "react";
 import "./InlineLoading.scss";
 import { CheckmarkFilled16, ErrorFilled16 } from "@carbon/icons-react";
 import IconLoading from "components/IconLoading";
+import { TFunction } from "i18next";
 
 export interface InlineLoadingProps {
   className?: string;
   status?: "default" | "submitting" | "submitted" | "error";
-  keyTranslate?: [string, string, string];
+  keyTranslate?: string;
+  translate?: TFunction;
 }
 
 const InlineLoading = React.forwardRef(
   (props: PropsWithChildren<InlineLoadingProps>, ref: React.Ref<any>) => {
-    const { className, status, keyTranslate } = props;
+    const { className, status, keyTranslate, translate } = props;
 
     return (
       <div className={classNames("inline-loading", className)}>
         {status === "submitting" && (
           <div className="submitting-box">
             <IconLoading color="#0F62FE" />
-            <div className="p-l--xxs">{keyTranslate[0]}</div>
+            <div className="p-l--xxs">
+              {keyTranslate && translate
+                ? translate(`${keyTranslate}.loading`)
+                : "Loading..."}
+            </div>
           </div>
         )}
         {status === "submitted" && (
           <div className="submitted-box">
             <CheckmarkFilled16 className="icon-submitted-inline-loading" />
-            {keyTranslate[1]}
+            {keyTranslate && translate
+              ? translate(`${keyTranslate}.submitted`)
+              : "Submitted"}
           </div>
         )}
         {status === "error" && (
           <div className="error-box">
             <ErrorFilled16 className="icon-error-inline-loading" />
-            {keyTranslate[2]}
+            {keyTranslate && translate
+              ? translate(`${keyTranslate}.error`)
+              : "Error"}
           </div>
         )}
       </div>
@@ -41,7 +51,6 @@ const InlineLoading = React.forwardRef(
 
 InlineLoading.defaultProps = {
   status: "submitting",
-  keyTranslate: ["loading...", "submitted", "error"],
 };
 
 export default InlineLoading;

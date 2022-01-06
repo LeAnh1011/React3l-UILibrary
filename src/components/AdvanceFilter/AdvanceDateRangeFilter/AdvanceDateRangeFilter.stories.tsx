f46fc2d1a4fdbf8f7290743c41a602017e5a270b
huongnt <huongnguyenhd96@gmail.com>
2022-01-06 11:28:24 +0700

    add advanced date range

---
+++ src/components/AdvanceFilter/AdvanceDateRangeFilter/AdvanceDateRangeFilter.stories.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import AdvanceDateRangeFilter from './AdvanceDateRangeFilter';
+import { Moment } from 'moment';
+import { Radio, RadioChangeEvent } from 'antd';
+import { BORDER_TYPE } from '../../../config/enum';
+
+export function AdvanceDateRangeFilterStories() {
+  const [value, setValue] = React.useState<[Moment, Moment]>([null, null]);
+
+  const [type, setType] = React.useState<BORDER_TYPE>(
+    BORDER_TYPE.FLOAT_LABEL
+  );
+
+  const [isDisabled, setIsDisabled] = React.useState(false);
+
+  const [isSmall, setIsSmall] = React.useState(false);
+
+
+  const handleChange = React.useCallback((dateMoment, dateString) => {
+    setValue(dateMoment);
+  }, []);
+
+  const handleChangeStyle = React.useCallback((event: RadioChangeEvent) => {
+    setType(event.target.value);
+  }, []);
+
+
+  const handleChangeSize = React.useCallback((event: RadioChangeEvent) => {
+    setIsSmall(event.target.value);
+  }, []);
+
+  const handleChangeDisabled = React.useCallback((event: RadioChangeEvent) => {
+    setIsDisabled(event.target.value);
+  }, []);
+
+  return <div style={{ margin: '10px', width: '250px' }}>
+    <AdvanceDateRangeFilter
+      type={type}
+      isSmall={isSmall}
+      disabled={isDisabled}
+      onChange={handleChange}
+      value={value} />
+
+    <div style={{ margin: "10px", width: "300px" }}>
+      <Radio.Group onChange={handleChangeStyle} value={type}>
+        <Radio value={BORDER_TYPE.MATERIAL}>Material</Radio>
+        <Radio value={BORDER_TYPE.FLOAT_LABEL}>Float Label</Radio>
+        <Radio value={BORDER_TYPE.BORDERED}>Bordered</Radio>
+      </Radio.Group>
+    </div>
+
+    <div style={{ margin: "10px", width: "300px" }}>
+      <Radio.Group onChange={handleChangeSize} value={isSmall}>
+        <Radio value={true}>Small</Radio>
+        <Radio value={false}>Default</Radio>
+      </Radio.Group>
+    </div>
+
+    <div style={{ margin: "10px", width: "300px" }}>
+      <Radio.Group onChange={handleChangeDisabled} value={isDisabled}>
+        <Radio value={true}>Disabled</Radio>
+        <Radio value={false}>Not Disabled</Radio>
+      </Radio.Group>
+    </div>
+  </div>;
+}

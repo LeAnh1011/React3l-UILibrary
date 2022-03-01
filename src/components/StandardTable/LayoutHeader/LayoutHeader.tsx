88334603127962a7d6d9c78f47eeb6dc628e3f19
khangdm <minhkhang231199@gmail.com>
2022-03-01 19:50:14 +0700

    update layout header with limitWord

--- src/components/StandardTable/LayoutHeader/LayoutHeader.tsx
+++ src/components/StandardTable/LayoutHeader/LayoutHeader.tsx
@@ -1,17 +1,26 @@
 import classNames from "classnames";
 import "./LayoutHeader.scss";
+import { Tooltip } from "antd";
+import { CommonService } from "services/common-service";
 
 export interface LayoutHeaderProps {
   className?: string;
   orderType?: "left" | "right" | "center";
   title?: string;
+  maxLength?: number;
 }
 
 function LayoutHeader(props: LayoutHeaderProps) {
-  const { className, orderType, title } = props;
+  const { className, orderType, title, maxLength } = props;
   return (
     <div className={classNames(className, `layout-header-${orderType}`)}>
-      {title}
+      {maxLength && title?.length > maxLength ? (
+        <Tooltip title={title}>
+          {CommonService.limitWord(title, maxLength)}
+        </Tooltip>
+      ) : (
+        title
+      )}
     </div>
   );
 }

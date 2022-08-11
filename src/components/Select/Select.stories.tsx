b9778c2a7799b1e6f0aba817588bf8c98019fcf1
HO.FPT.VN\HuongNT242 <huongnt242@fpt.com.vn>
2022-08-11 10:47:09 +0700

    change story select

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -2,8 +2,10 @@ import { storiesOf } from "@storybook/react";
 import { EnumSelectStories } from "./EnumSelect/EnumSelect.stories";
 import { MultipleSelectStories } from "./MultipleSelect/MultipleSelect.stories";
 import { SingleSelectStories } from "./SingleSelect/Select.stories";
+import { TreeSelectStories } from "./TreeSelect/TreeSelect.stories";
 
 storiesOf("Select", module)
   .add("SingleSelectStories", SingleSelectStories)
   .add("MultipleSelectStories", MultipleSelectStories)
-  .add("EnumSelectStories", EnumSelectStories);
+  .add("EnumSelectStories", EnumSelectStories)
+  .add("TreeSelectStories", TreeSelectStories);

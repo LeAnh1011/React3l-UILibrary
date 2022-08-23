a56f7b4db8974cbc18e6e216826946a2b168ba19
HO.FPT.VN\HuongNT242 <huongnt242@fpt.com.vn>
2022-08-23 10:30:58 +0700

    add Inputsearch story

--- src/components/Input/InputSearch/InputSearch.stories.tsx
+++ src/components/Input/InputSearch/InputSearch.stories.tsx
@@ -1,46 +1,34 @@
+import { Story } from "@storybook/react";
 import React from "react";
 import { IdFilter, StringFilter } from "react3l-advanced-filters";
-import { Model, ModelFilter } from "react3l-common";
+import { ModelFilter } from "react3l-common";
 import InputSearch from "./InputSearch";
-import { Observable, of } from "rxjs";
 
-export class Demo extends Model {
-  id: number;
-  name: string;
-  code: string;
-}
-
-export class DemoFilter extends ModelFilter {
+class DemoFilter extends ModelFilter {
   id: IdFilter = new IdFilter();
   name: StringFilter = new StringFilter();
   code: StringFilter = new StringFilter();
 }
-const demoListEnum = (TModelFilter?: ModelFilter): Observable<Demo[]> => {
-  return of([
-    {
-      id: 1,
-      name: "Option 2 very long one",
-      code: "E1",
+
+export default {
+  title: "Input/InputSearch",
+  component: InputSearch,
+  parameters: {
+    controls: { expanded: true },
+  },
+  argTypes: {
+    placeHolder: {
+      control: "text",
+      defaultValue: "Search...",
     },
-    { id: 2, name: "Enum 2", code: "E2" },
-    { id: 3, name: "Enum 3", code: "E3" },
-    { id: 4, name: "Enum 4", code: "E4" },
-    { id: 5, name: "Enum 5", code: "E5" },
-  ]);
+  },
+  value: {
+    control: "text",
+    defaultValue: "hello"
+  }
 };
 
-export function InputSearchStories() {
-  const [selectModelFilter] = React.useState<DemoFilter>(new DemoFilter());
-  const [, setSelectModel] = React.useState<Model>({});
-  const [, setValue] = React.useState<String>("hello");
-
-  const handleSetModel = React.useCallback((...[, item]) => {
-    setSelectModel(item);
-  }, []);
-
-  const handleSearchChange = React.useCallback((value) => {
-    setValue(value);
-  }, []);
+const Template: Story = (args) => {
 
   return (
     <>
@@ -52,32 +40,13 @@ export function InputSearchStories() {
         }}
       >
         <InputSearch
+          {...args}
           classFilter={DemoFilter}
-          placeHolder="Search..."
-          onChange={handleSearchChange}
           className="m-b--xl"
-        />
-
-        <InputSearch
-          valueFilter={selectModelFilter}
-          searchProperty={"name"}
-          classFilter={DemoFilter}
-          placeHolder="Search..."
-          getList={demoListEnum}
-          onChangeSearchField={handleSetModel}
-          className="m-b--xl"
-        />
-
-        <InputSearch
-          valueFilter={selectModelFilter}
-          searchProperty={"name"}
-          classFilter={DemoFilter}
-          placeHolder="Search..."
-          getList={demoListEnum}
-          onChangeSearchField={handleSetModel}
-          animationInput={false}
         />
       </div>
     </>
   );
-}
+};
+
+export const Default = Template.bind({});

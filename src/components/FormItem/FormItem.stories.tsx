f842dc0f152efbbe5dbf082986a7cb2317481236
HO.FPT.VN\HuongNT242 <huongnt242@fpt.com.vn>
2022-09-05 11:02:18 +0700

    add form Item storybook

--- src/components/FormItem/FormItem.stories.tsx
+++ src/components/FormItem/FormItem.stories.tsx
@@ -1,39 +1,74 @@
-import { storiesOf } from "@storybook/react";
+import React from "react";
+import { storiesOf, Story } from "@storybook/react";
 import FormItem from "./FormItem";
 import InputText from "../Input/InputText/InputText";
 import { ValidateStatus } from "./../../config/enum";
+import {
+  ArgsTable,
+  Description,
+  Primary,
+  PRIMARY_STORY,
+  Stories,
+  Subtitle,
+  Title
+} from "@storybook/addon-docs";
 
-function Default() {
+export default {
+  title: "FormItem",
+  component: FormItem,
+  parameters: {
+    controls: { expanded: true },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Description />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
+    },
+  },
+  argTypes: {
+    message: {
+      defaultValue: "Field required!",
+    },
+   
+  },
+ 
+};
+
+
+const Template: Story = (args) => {
   return (
     <>
       <div style={{ margin: "10px", width: "250px" }}>
         <FormItem
-          validateStatus={ValidateStatus.error}
-          message={"Field required!"}
+        {...args}
         >
           <InputText placeHolder={"Enter text..."} />
         </FormItem>
       </div>
       <div style={{ margin: "10px", width: "250px" }}>
         <FormItem
-          validateStatus={ValidateStatus.warning}
-          message={"Field required!"}
+        {...args}
         >
           <InputText placeHolder={"Enter text..."} />
         </FormItem>
       </div>
       <div style={{ margin: "10px", width: "250px" }}>
         <FormItem
-          validateStatus={ValidateStatus.success}
-          message={"Field required!"}
+        {...args}
         >
           <InputText placeHolder={"Enter text..."} />
         </FormItem>
       </div>
       <div style={{ margin: "10px", width: "250px" }}>
         <FormItem
-          validateStatus={ValidateStatus.validating}
-          message={"Field required!"}
+        {...args}
         >
           <InputText placeHolder={"Enter text..."} />
         </FormItem>
@@ -41,5 +76,4 @@ function Default() {
     </>
   );
 }
-
-storiesOf("FormItem", module).add("Default", Default);
+export const Default = Template.bind({});

019f72ee056801dab0e9530cce15b91aa6202965
khangdm <minhkhang231199@gmail.com>
2023-04-19 11:17:08 +0700

    update advanceStringFilter story

--- src/components/AdvanceFilter/AdvanceStringFilter/AdvanceStringFilter.tsx
+++ src/components/AdvanceFilter/AdvanceStringFilter/AdvanceStringFilter.tsx
@@ -5,23 +5,36 @@ import React, { ReactSVGElement, RefObject } from "react";
 import "./AdvanceStringFilter.scss";
 
 export interface AdvanceStringFilterProps {
+  /** Label for filter field */
   label?: string;
+  /** Control the style type of component: MATERIAL, BORDERED, FLOAT_LABEL  */
   type?: BORDER_TYPE;
-  floatLabel?: boolean;
-  isMaterial?: boolean;
+  /** Prefix for filter value */
   prefix?: string | JSX.Element;
+  /** Suffix for filter value */
   suffix?: string | JSX.Element;
+  /** User-filled value */
   value?: string;
+  /** Not allow to handle change filter */
   disabled?: boolean;
+  /** Placeholder of the component */
   placeHolder?: string;
+  /** Use to custom style the component */
   className?: string;
+  /** Set maximum number of characters value filter */
   maxLength?: number;
+  /** Control the size of the component */
   isSmall?: boolean;
-  bgColor?: "white" | "gray";
+  /** Handle the change value filter of the component */
   onChange?: (T: string | null) => void;
+  /** Handle onEnter action */
   onEnter?: (T: string | null) => void;
+  /** Handle onBlur action */
   onBlur?: (T: string | null) => void;
+  /** Handle onKeyDown action */
   onKeyDown?: (event: any) => void;
+  /** Custom background color for component: "white" || "gray" */
+  bgColor?: "white" | "gray";
 }
 
 const AdvanceStringFilter = React.forwardRef(

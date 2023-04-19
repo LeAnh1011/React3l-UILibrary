3386377c361c9f5458481a2555436d42690c6b94
khangdm <minhkhang231199@gmail.com>
2023-04-19 16:52:58 +0700

    update InputNumber story

--- src/components/Input/InputNumber/InputNumber.stories.tsx
+++ src/components/Input/InputNumber/InputNumber.stories.tsx
@@ -1,172 +1,108 @@
 import React from "react";
-import InputNumber, { DECIMAL, LONG } from "./InputNumber";
-import { Radio } from "antd";
-import { RadioChangeEvent } from "antd/lib/radio";
 import FormItem from "../../FormItem/FormItem";
-import { ValidateStatus } from "./../../../config/enum";
 import { BORDER_TYPE } from "./../../../config/enum";
-import Apple16 from "@carbon/icons-react/es/apple/16";
+import InputNumber, { DECIMAL, LONG } from "./InputNumber";
+import {
+  ArgsTable,
+  Description,
+  Primary,
+  PRIMARY_STORY,
+  Stories,
+  Subtitle,
+  Title,
+} from "@storybook/addon-docs";
+import { ComponentMeta, Story } from "@storybook/react";
 
-export function InputNumberStories() {
-  const [numberType, setNumberType] = React.useState<string>(LONG);
+export default {
+  title: "Input/InputNumber",
+  component: InputNumber,
+  subcomponents: { FormItem },
+  parameters: {
+    controls: { expanded: true },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Description />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
+    },
+  },
+  argTypes: {
+    label: {
+      control: "text",
+      defaultValue: "First Name",
+    },
+    placeHolder: {
+      control: "text",
+      defaultValue: "Enter number",
+    },
+    type: {
+      control: {
+        type: "radio",
+        options: [
+          BORDER_TYPE.MATERIAL,
+          BORDER_TYPE.FLOAT_LABEL,
+          BORDER_TYPE.BORDERED,
+        ],
+      },
+      defaultValue: 0,
+    },
+    numberType: {
+      control: { type: "radio", options: [LONG, DECIMAL] },
+      defaultValue: LONG,
+    },
+    isRequired: {},
+    value: {},
+    prefix: {
+      defaultValue: "Mr.",
+    },
+    suffix: {
+      defaultValue: "Kg",
+    },
+    allowPositive: {},
+    isReverseSymb: {},
+    decimalDigit: {},
+    disabled: {},
+    min: {},
+    max: {},
+    action: {},
+    isSmall: {},
+    onChange: {},
+    onBlur: {},
+    onEnter: {},
+    className: {},
+  },
+} as ComponentMeta<typeof InputNumber>;
 
-  const [type, setType] = React.useState<BORDER_TYPE>(BORDER_TYPE.BORDERED);
-
-  const [isReverse, setIsReverse] = React.useState(false);
-
-  const [isNegative, setIsNegative] = React.useState(false);
-
-  const [isSmall, setIsSmall] = React.useState(false);
-
+const Template: Story = (args) => {
   const [iconName] = React.useState("");
 
-  const [isDisabled, setIsDisabled] = React.useState(false);
-
-  const [isValidated, setValidated] = React.useState(false);
-
   const [value, setValue] = React.useState();
-
-  const handleChangeValidated = React.useCallback((event: RadioChangeEvent) => {
-    setValidated(event.target.value);
-  }, []);
-
-  const handleChangeDisabled = React.useCallback((event: RadioChangeEvent) => {
-    setIsDisabled(event.target.value);
-  }, []);
-
-  const handleChangeStyle = React.useCallback((event: RadioChangeEvent) => {
-    setType(event.target.value);
-  }, []);
 
   const handleChangeValue = React.useCallback((value) => {
     setValue(value);
   }, []);
 
-  const handleChangeType = React.useCallback(
-    (event: RadioChangeEvent) => {
-      setNumberType(event.target.value);
-      setValue(undefined);
-    },
-    [setValue]
-  );
-
-  const handleChangeSeperation = React.useCallback(
-    (event: RadioChangeEvent) => {
-      setIsReverse(event.target.value);
-      setValue(undefined);
-    },
-    [setValue]
-  );
-
-  const handleChangePositive = React.useCallback(
-    (event: RadioChangeEvent) => {
-      setIsNegative(event.target.value);
-      setValue(undefined);
-    },
-    [setValue]
-  );
-
-  const handleChangeSize = React.useCallback((event: RadioChangeEvent) => {
-    setIsSmall(event.target.value);
-  }, []);
-
   return (
     <div style={{ width: "300px", margin: "10px" }}>
       <div style={{ margin: "15px 0" }}>
-        <FormItem
-          message="Helper"
-          validateStatus={isValidated ? ValidateStatus.error : null}
-        >
+        <FormItem>
           <InputNumber
-            type={type}
-            label="Label"
-            prefix="Age"
-            placeHolder={"Enter number..."}
+            {...args}
             className={iconName}
             value={value}
             onChange={handleChangeValue}
-            numberType={numberType}
-            isReverseSymb={isReverse}
-            allowNegative={isNegative}
-            isSmall={isSmall}
-            disabled={isDisabled}
-            action={{
-              name: "Help",
-              action: () => {},
-            }}
-            bgColor="white"
           />
         </FormItem>
-      </div>
-      <div style={{ margin: "15px 0" }}>
-        <FormItem
-          message="Helper"
-          validateStatus={isValidated ? ValidateStatus.error : null}
-        >
-          <InputNumber
-            type={type}
-            label="Label"
-            prefix={<Apple16 />}
-            suffix="Age"
-            placeHolder={"Enter number..."}
-            className={iconName}
-            value={value}
-            onChange={handleChangeValue}
-            numberType={numberType}
-            isReverseSymb={isReverse}
-            allowNegative={isNegative}
-            isSmall={isSmall}
-            disabled={isDisabled}
-            action={{
-              name: "Help",
-              action: () => {},
-            }}
-          />
-        </FormItem>
-      </div>
-      <div style={{ margin: "10px", width: "400px" }}>
-        <Radio.Group onChange={handleChangeStyle} value={type}>
-          <Radio value={BORDER_TYPE.MATERIAL}>Material</Radio>
-          <Radio value={BORDER_TYPE.FLOAT_LABEL}>Float Label</Radio>
-          <Radio value={BORDER_TYPE.BORDERED}>Bordered</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeType} value={numberType}>
-          <Radio value={DECIMAL}>Decimal</Radio>
-          <Radio value={LONG}>Long</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeSeperation} value={isReverse}>
-          <Radio value={true}>Reverse Seperation</Radio>
-          <Radio value={false}>Normal</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangePositive} value={isNegative}>
-          <Radio value={true}>Allow Nagative</Radio>
-          <Radio value={false}>Not Allow Nagative</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeSize} value={isSmall}>
-          <Radio value={true}>Small</Radio>
-          <Radio value={false}>Default</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeDisabled} value={isDisabled}>
-          <Radio value={true}>Disabled</Radio>
-          <Radio value={false}>Not Disabled</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeValidated} value={isValidated}>
-          <Radio value={true}>Validated</Radio>
-          <Radio value={false}>Not Validated</Radio>
-        </Radio.Group>
       </div>
     </div>
   );
-}
+};
+
+export const Default = Template.bind({});
--- src/components/Input/InputNumber/InputNumber.tsx
+++ src/components/Input/InputNumber/InputNumber.tsx
@@ -14,27 +14,48 @@ interface InputNumberAction {
 }
 
 export interface InputNumberProps {
+  /**Label for filter field*/
   label?: string;
+  /**Show symbol * as required field*/
   isRequired?: boolean;
+  /**Control the style type of component: MATERIAL, BORDERED, FLOAT_LABEL */
   type?: BORDER_TYPE;
+  /**User-filled value*/
   value?: number;
+  /**Prefix for filter value*/
   prefix?: string | ReactNode;
+  /**Suffix for filter value*/
   suffix?: string | ReactNode;
+  /**Allow value to be negative or not*/
   allowNegative?: boolean;
+  /**Provide an option set decimal number type for value*/
   numberType?: string;
+  /**Reverse symbol “.” and “,”*/
   isReverseSymb?: boolean;
+  /**Provide a length of number behind the point (character)*/
   decimalDigit?: number;
+  /**Placeholder of the component*/
   placeHolder?: string;
+  /**Not allow to handle change value*/
   disabled?: boolean;
+  /**Use to custom style the component*/
   className?: string;
+  /**Min of the value number*/
   min?: number;
+  /**Max of the value number*/
   max?: number;
+  /** Provide a custom action (onClick) to the component */
   action?: InputNumberAction;
+  /**Control the size of the component*/
   isSmall?: boolean;
-  bgColor?: "white" | "gray";
+  /**Handle the change value of the component*/
   onChange?: (T: number) => void;
+  /**Handle onEnter action*/
   onEnter?: (T: number) => void;
+  /**Handle onBlur action*/
   onBlur?: (T: number) => void;
+  /** Custom background color for component: "white" || "gray" */
+  bgColor?: "white" | "gray";
 }
 
 function InputNumber(props: InputNumberProps) {

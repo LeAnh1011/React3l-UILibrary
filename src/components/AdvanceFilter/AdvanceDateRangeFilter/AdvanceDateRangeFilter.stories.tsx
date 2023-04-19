32fdcdd758898c29b4273d26b737bb48774392af
khangdm <minhkhang231199@gmail.com>
2023-04-19 10:07:06 +0700

    update advaceDateRangeFilter story

--- src/components/AdvanceFilter/AdvanceDateRangeFilter/AdvanceDateRangeFilter.stories.tsx
+++ src/components/AdvanceFilter/AdvanceDateRangeFilter/AdvanceDateRangeFilter.stories.tsx
@@ -1,87 +1,86 @@
 import React from "react";
 import AdvanceDateRangeFilter from "./AdvanceDateRangeFilter";
-import { Radio, RadioChangeEvent } from "antd";
-import { BORDER_TYPE, ValidateStatus } from "../../../config/enum";
+import { BORDER_TYPE } from "../../../config/enum";
 import FormItem from "../../FormItem/FormItem";
+import {
+  ArgsTable,
+  Description,
+  Primary,
+  PRIMARY_STORY,
+  Stories,
+  Subtitle,
+  Title,
+} from "@storybook/addon-docs";
+import { Story } from "@storybook/react";
 
-export function AdvanceDateRangeFilterStories() {
+export default {
+  title: "AdvanceFilter/AdvanceDateRangeFilter",
+  component: AdvanceDateRangeFilter,
+  subcomponents: { AdvanceDateRangeFilter },
+  parameters: {
+    controls: { expanded: true },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Description />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
+    },
+  },
+  argTypes: {
+    label: {
+      control: "text",
+      defaultValue: "Ngày tạo",
+    },
+    type: {
+      control: {
+        type: "radio",
+        options: [
+          BORDER_TYPE.MATERIAL,
+          BORDER_TYPE.BORDERED,
+          BORDER_TYPE.FLOAT_LABEL,
+        ],
+      },
+      defaultValue: 0,
+    },
+    value: {},
+    dateFormat: {},
+    className: {},
+    isSmall: {},
+    isRequired: {},
+    disabled: {},
+    placeholder: {},
+    onChange: {},
+    action: {},
+  },
+};
+
+const Template: Story = (args) => {
   const [value, setValue] = React.useState<[any, any]>([null, null]);
-
-  const [isValidated, setValidated] = React.useState(false);
-
-  const [type, setType] = React.useState<BORDER_TYPE>(BORDER_TYPE.FLOAT_LABEL);
-
-  const [isDisabled, setIsDisabled] = React.useState(false);
-
-  const [isSmall, setIsSmall] = React.useState(false);
-
-  const handleChangeValidated = React.useCallback((event: RadioChangeEvent) => {
-    setValidated(event.target.value);
-  }, []);
 
   const handleChange = React.useCallback((dateMoment, dateString) => {
     setValue(dateMoment);
   }, []);
 
-  const handleChangeStyle = React.useCallback((event: RadioChangeEvent) => {
-    setType(event.target.value);
-  }, []);
-
-  const handleChangeDisabled = React.useCallback((event: RadioChangeEvent) => {
-    setIsDisabled(event.target.value);
-  }, []);
-
-  const handleChangeSize = React.useCallback((event: RadioChangeEvent) => {
-    setIsSmall(event.target.value);
-  }, []);
-
   return (
     <div style={{ margin: "10px", width: "300px" }}>
       <div style={{ margin: "10px", width: "300px" }}>
-        <FormItem
-          validateStatus={isValidated ? ValidateStatus.error : null}
-          message={"Helper text"}
-        >
+        <FormItem message={"Helper text"}>
           <AdvanceDateRangeFilter
-            type={type}
-            isSmall={isSmall}
-            label="Ngày nhập hàng"
-            placeholder={["Chọn ngày ...", "Chọn ngày ..."]}
+            {...args}
             onChange={handleChange}
             values={value}
-            disabled={isDisabled}
           />
         </FormItem>
       </div>
-
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeStyle} value={type}>
-          <Radio value={BORDER_TYPE.MATERIAL}>Material</Radio>
-          <Radio value={BORDER_TYPE.FLOAT_LABEL}>Float Label</Radio>
-          <Radio value={BORDER_TYPE.BORDERED}>Bordered</Radio>
-        </Radio.Group>
-      </div>
-
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeSize} value={isSmall}>
-          <Radio value={true}>Small</Radio>
-          <Radio value={false}>Default</Radio>
-        </Radio.Group>
-      </div>
-
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeValidated} value={isValidated}>
-          <Radio value={true}>Validated</Radio>
-          <Radio value={false}>Not Validated</Radio>
-        </Radio.Group>
-      </div>
-
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeDisabled} value={isDisabled}>
-          <Radio value={true}>Disabled</Radio>
-          <Radio value={false}>Not Disabled</Radio>
-        </Radio.Group>
-      </div>
     </div>
   );
-}
+};
+
+export const Default = Template.bind({});

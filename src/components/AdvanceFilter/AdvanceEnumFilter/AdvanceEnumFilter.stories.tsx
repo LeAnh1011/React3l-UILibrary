b265a91026da6798cda8ae2632afbb7d6366c153
khangdm <minhkhang231199@gmail.com>
2023-04-19 10:15:05 +0700

    update advanceEnumFilter

--- src/components/AdvanceFilter/AdvanceEnumFilter/AdvanceEnumFilter.stories.tsx
+++ src/components/AdvanceFilter/AdvanceEnumFilter/AdvanceEnumFilter.stories.tsx
@@ -1,12 +1,20 @@
-import { Radio } from "antd";
-import { RadioChangeEvent } from "antd/lib/radio";
 import React from "react";
 import { IdFilter, StringFilter } from "react3l-advanced-filters";
 import { Model, ModelFilter } from "react3l-common";
 import { Observable } from "rxjs";
 import FormItem from "../../FormItem/FormItem";
-import { BORDER_TYPE, ValidateStatus } from "./../../../config/enum";
+import { BORDER_TYPE } from "./../../../config/enum";
 import AdvanceEnumFilter from "./AdvanceEnumFilter";
+import { Story } from "@storybook/react";
+import {
+  ArgsTable,
+  Description,
+  Primary,
+  PRIMARY_STORY,
+  Stories,
+  Subtitle,
+  Title,
+} from "@storybook/addon-docs";
 
 export class DemoFilter extends ModelFilter {
   id: IdFilter = new IdFilter();
@@ -38,32 +46,71 @@ const demoSearchFunc = () => {
   return demoObservable;
 };
 
-export function AdvanceEnumFilterStories() {
+export default {
+  title: "AdvanceFilter/AdvanceEnumFilter",
+  component: AdvanceEnumFilter,
+  subcomponents: { FormItem },
+  parameters: {
+    controls: { expanded: true },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Description />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
+    },
+  },
+  argTypes: {
+    label: {
+      control: "text",
+      defaultValue: "Đơn vị tổ chức",
+    },
+    placeHolder: {
+      defaultValue: "Chọn đơn vị",
+    },
+    type: {
+      control: {
+        type: "radio",
+        options: [
+          BORDER_TYPE.MATERIAL,
+          BORDER_TYPE.BORDERED,
+          BORDER_TYPE.FLOAT_LABEL,
+        ],
+      },
+      defaultValue: 1,
+    },
+    value: {},
+    listValue: {},
+    disabled: {},
+    appendToBody: {},
+    onChange: {},
+    onChangeMultiple: {},
+    render: {},
+    selectWithAdd: {},
+    isSmall: {},
+    preferOptions: {},
+    isMultiple: {},
+    getList: {},
+    height: {},
+  },
+};
+
+const Template: Story = (args) => {
   const [selectModel, setSelectModel] = React.useState<Model>({
     id: 0,
     name: "Option 2",
     code: "FAD",
   });
 
-  const [type, setType] = React.useState<BORDER_TYPE>(BORDER_TYPE.BORDERED);
-
-  const [isValidated, setValidated] = React.useState(false);
-
-  const [isSelectWithAdd, setIsSelectWithAdd] = React.useState<boolean>(false);
-
-  const [isDisabled, setIsDisabled] = React.useState<boolean>(false);
-
-  const [isSmall, setIsSmall] = React.useState<boolean>(false);
-
-  const [isMultiple, setIsMultiple] = React.useState<boolean>(false);
-
   const [multifilter, setFilter] = React.useState(new DemoFilter());
 
   const [list, setList] = React.useState<[]>([]);
-
-  const handleChangeStyle = React.useCallback((event: RadioChangeEvent) => {
-    setType(event.target.value);
-  }, []);
 
   const handleSetModel = React.useCallback((...[, item]) => {
     setSelectModel(item);
@@ -77,29 +124,6 @@ export function AdvanceEnumFilterStories() {
     }
   }, []);
 
-  const handleChangeValidated = React.useCallback((event: RadioChangeEvent) => {
-    setValidated(event.target.value);
-  }, []);
-
-  const handleChangeSelectWithAdd = React.useCallback(
-    (event: RadioChangeEvent) => {
-      setIsSelectWithAdd(event.target.value);
-    },
-    []
-  );
-
-  const handleChangeDisabled = React.useCallback((event: RadioChangeEvent) => {
-    setIsDisabled(event.target.value);
-  }, []);
-
-  const handleChangeSize = React.useCallback((event: RadioChangeEvent) => {
-    setIsSmall(event.target.value);
-  }, []);
-
-  const handleChangeMultipe = React.useCallback((event: RadioChangeEvent) => {
-    setIsMultiple(event.target.value);
-  }, []);
-
   const handleChangeFilter = React.useCallback(
     (list, ids) => {
       setFilter({ ...multifilter, id: { in: ids } });
@@ -111,73 +135,20 @@ export function AdvanceEnumFilterStories() {
   return (
     <div style={{ margin: "10px", width: "300px" }}>
       <div style={{ margin: "10px", width: "300px" }}>
-        <FormItem
-          validateStatus={isValidated ? ValidateStatus.error : null}
-          message={isValidated ? "Error label" : ""}
-        >
+        <FormItem message={"Error label"}>
           <AdvanceEnumFilter
-            placeHolder={"Select Organization"}
+            {...args}
             value={selectModel}
             render={handleRenderModel}
             onChange={handleSetModel}
             getList={demoSearchFunc}
-            type={type}
-            label={"Label"}
-            disabled={isDisabled}
-            isSmall={isSmall}
-            isMultiple={isMultiple}
-            onChangeMultiple={handleChangeFilter}
-            listValue={list}
-            height={500}
+            onChangeMultiple={handleChangeFilter} // if type is multiple pass this props
+            listValue={list} // if type is multiple pass this prop
           />
         </FormItem>
       </div>
-
-      <div style={{ margin: "10px", width: "400px" }}>
-        <Radio.Group onChange={handleChangeStyle} value={type}>
-          <Radio value={BORDER_TYPE.MATERIAL}>Material</Radio>
-          <Radio value={BORDER_TYPE.FLOAT_LABEL}>Float Label</Radio>
-          <Radio value={BORDER_TYPE.BORDERED}>Bordered</Radio>
-        </Radio.Group>
-      </div>
-
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeValidated} value={isValidated}>
-          <Radio value={true}>Validated</Radio>
-          <Radio value={false}>Not Validated</Radio>
-        </Radio.Group>
-      </div>
-
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeDisabled} value={isDisabled}>
-          <Radio value={true}>Disabled</Radio>
-          <Radio value={false}>Not Disabled</Radio>
-        </Radio.Group>
-      </div>
-
-      <div style={{ margin: "10px", width: "400px" }}>
-        <Radio.Group
-          onChange={handleChangeSelectWithAdd}
-          value={isSelectWithAdd}
-        >
-          <Radio value={true}>Select with add</Radio>
-          <Radio value={false}>Not select with add</Radio>
-        </Radio.Group>
-      </div>
-
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeSize} value={isSmall}>
-          <Radio value={true}>Small</Radio>
-          <Radio value={false}>Default</Radio>
-        </Radio.Group>
-      </div>
-
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeMultipe} value={isMultiple}>
-          <Radio value={true}>Multiple</Radio>
-          <Radio value={false}>Single</Radio>
-        </Radio.Group>
-      </div>
     </div>
   );
-}
+};
+
+export const Default = Template.bind({});
--- src/components/AdvanceFilter/AdvanceEnumFilter/AdvanceEnumFilter.tsx
+++ src/components/AdvanceFilter/AdvanceEnumFilter/AdvanceEnumFilter.tsx
@@ -10,36 +10,37 @@ import { CommonService } from "@Services/common-service";
 import { ErrorObserver, Observable, Subscription } from "rxjs";
 
 export interface AdvanceEnumProps<T extends Model> {
+  /**Value users select*/
   value?: Model;
-
+  /**List value users select*/
   listValue?: Model[];
-
+  /**Placeholder of the component*/
   placeHolder?: string;
-
+  /**Not allow to handle change the component*/
   disabled?: boolean;
-
+  /**Append this component to body*/
   appendToBody?: boolean;
-
+  /**Handle the change value of the component*/
   onChange?: (id: number, T?: T) => void;
-
+  /**Handle the change list value of the component */
   onChangeMultiple?: (selectedList?: T[], ids?: []) => void;
-
+  /**Provide a function to render a specific property as name*/
   render?: (t: T) => string;
-
+  /**Control the style type of component: MATERIAL, BORDERED, FLOAT_LABEL */
   type?: BORDER_TYPE;
-
+  /**Label for filter field*/
   label?: string;
-
+  /**Control the size of the component*/
   isSmall?: boolean;
-
+  /**Prefer option filter to select*/
   preferOptions?: T[];
-
+  /**Option to select multiple*/
   isMultiple?: boolean;
-
+  /**Api to get list data filter*/
   getList?: () => Observable<T[]>;
-
+  /**Custom height of dropdown data list*/
   height?: number;
-
+  /** Custom background color for component: "white" || "gray" */
   bgColor?: "white" | "gray";
 }
 

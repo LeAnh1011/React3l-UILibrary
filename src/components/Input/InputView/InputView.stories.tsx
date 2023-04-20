6f2a5ac9eda3967ba12343c8c05ae796dc60dad7
khangdm <minhkhang231199@gmail.com>
2023-04-20 09:50:38 +0700

    update inputView story

--- src/components/Input/InputView/InputView.stories.tsx
+++ src/components/Input/InputView/InputView.stories.tsx
@@ -1,59 +1,77 @@
-import { Radio, RadioChangeEvent } from "antd";
+import {
+  ArgsTable,
+  Description,
+  PRIMARY_STORY,
+  Primary,
+  Stories,
+  Subtitle,
+  Title,
+} from "@storybook/addon-docs";
+import { Story } from "@storybook/react";
 import React from "react";
-import { StringFilter } from "react3l-advanced-filters";
-import { ModelFilter } from "react3l-common";
+import FormItem from "../../FormItem/FormItem";
 import InputView, { BORDER_TYPE } from "./InputView";
 
-export class DemoFilter extends ModelFilter {
-  name: StringFilter = new StringFilter();
-  code: StringFilter = new StringFilter();
-}
+export default {
+  title: "Input/InputView",
+  component: InputView,
+  parameters: {
+    controls: { expanded: true },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Description />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
+    },
+  },
+  argTypes: {
+    label: {
+      control: "text",
+      defaultValue: "First Name",
+    },
+    maxLength: {
+      control: "number",
+    },
+    placeHolder: {
+      control: "text",
+      defaultValue: "Enter text",
+    },
+    type: {
+      control: {
+        type: "radio",
+        options: [
+          BORDER_TYPE.MATERIAL,
+          BORDER_TYPE.BORDERED,
+          BORDER_TYPE.FLOAT_LABEL,
+          BORDER_TYPE.NOT_BORDERED,
+        ],
+      },
+      defaultValue: 1,
+    },
+  },
+};
 
-export function InputViewStories() {
+const Template: Story = (args) => {
   const [inputValue] = React.useState(
     "asfdsadasdfv asfdsadasdfv asfdsadasdfv asfdsadasdfv asfdsadasdfvasfdsadasdfv asfdsadasdfv asfdsadasdfv"
   );
 
-  const [type, setType] = React.useState<BORDER_TYPE>(BORDER_TYPE.BORDERED);
-
-  const [isSmall, setIsSmall] = React.useState(false);
-
-  const handleChangeStyle = React.useCallback((event: RadioChangeEvent) => {
-    setType(event.target.value);
-  }, []);
-
-  const handleChangeSize = React.useCallback((event: RadioChangeEvent) => {
-    setIsSmall(event.target.value);
-  }, []);
-
   return (
     <div style={{ width: "300px", padding: "10px" }}>
       <div style={{ margin: "15px 0" }}>
-        <InputView
-          type={type}
-          isSmall={isSmall}
-          label="First Name"
-          showCount={true}
-          maxLength={100}
-          value={inputValue}
-          placeHolder={"Enter text..."}
-        />
-      </div>
-
-      <div style={{ margin: "10px", width: "400px" }}>
-        <Radio.Group onChange={handleChangeStyle} value={type}>
-          <Radio value={BORDER_TYPE.MATERIAL}>Material</Radio>
-          <Radio value={BORDER_TYPE.FLOAT_LABEL}>Float Label</Radio>
-          <Radio value={BORDER_TYPE.BORDERED}>Bordered</Radio>
-          <Radio value={BORDER_TYPE.NOT_BORDERED}>Not Bordered</Radio>
-        </Radio.Group>
-      </div>
-      <div style={{ margin: "10px", width: "300px" }}>
-        <Radio.Group onChange={handleChangeSize} value={isSmall}>
-          <Radio value={true}>Small</Radio>
-          <Radio value={false}>Default</Radio>
-        </Radio.Group>
+        <FormItem>
+          <InputView {...args} label="First Name" value={inputValue} />
+        </FormItem>
       </div>
     </div>
   );
-}
+};
+
+export const Default = Template.bind({});

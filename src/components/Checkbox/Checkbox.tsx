63055bde1ebab49c6e3e9ba1bf7ea178f24d114b
khangdm <minhkhang231199@gmail.com>
2024-05-20 22:43:23 +0700

    update maxlengthItem for label checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Checkbox as CheckboxAntd } from "antd";
+import { Checkbox as CheckboxAntd, Tooltip } from "antd";
 import CheckboxGroup from "./CheckboxGroup";
 import classNames from "classnames";
 import "./Checkbox.scss";
+import { CommonService } from "@Services/common-service";
 
 export interface CheckboxProps {
   /**Handle change value checkbox */
@@ -15,10 +16,12 @@ export interface CheckboxProps {
   label?: string;
   /**Type read only is true */
   readOnly?: boolean;
+  /**Max length of label checkbox */
+  maxLengthItem?: number;
 }
 
 function Checkbox(props: CheckboxProps) {
-  const { onChange, checked, disabled, label, readOnly } = props;
+  const { onChange, checked, disabled, label, readOnly, maxLengthItem } = props;
   const handleChange = React.useCallback(
     (value) => {
       if (onChange && typeof onChange === "function") {
@@ -40,7 +43,13 @@ function Checkbox(props: CheckboxProps) {
         onChange={handleChange}
         disabled={disabled}
       >
-        {label}
+        {maxLengthItem && label?.length > maxLengthItem ? (
+          <Tooltip title={label}>
+            {CommonService.limitWord(label, maxLengthItem)}
+          </Tooltip>
+        ) : (
+          label
+        )}
       </CheckboxAntd>
     </div>
   );
